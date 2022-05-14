02f11a35c57be52472458fb7d102e360d4fea274
NNEKUS <104697289+NNEKUS@users.noreply.github.com>
2022-05-14 16:35:11 +0100

    Update api_exercise.py

--- data-engineering/storing-big-data/apis/api_exercise.py
+++ data-engineering/storing-big-data/apis/api_exercise.py
@@ -28,6 +28,27 @@ LOCATION_ID = "252556" # <-- Obtained from the AccuWeather website
 # Here we create a function that takes in our credentials and
 # location ID to make the request using `urllib`.
 def get_weather(api_key, location_id):
+    url = 'http://dataservice.accuweather.com/currentconditions/v1/%s?apikey=%s&details=true' % (location_id, api)
+    print(url)
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+    print(data)
+    return (data[0]['Temperature']['Imperial']['Value'],
+            data[0]['RelativeHumidity'],
+            data[0]['Wind']['Direction']['Degrees'],
+            data[0]['Wind']['Speed']['Imperial']['Value'],
+            data[0]['UVIndex'],
+            data[0]['CloudCover'],
+            data[0]['Pressure']['Metric']['Value'],
+            data[0]['Precip1hr']['Metric']['Value'],
+            data)
+
+
+timestamp = time.time()
+
+temperature, humidity, wind_bearing, wind_speed, uv_index, cloud_cover, pressure, precipitation, raw \
+    = get_weather(API, LOCATION_ID)
+
     """Retrieves the current weather conditions for a specified geographic 
        location using the AccuWeather API.  
 
